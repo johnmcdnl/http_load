a7d451679ed93517c2b19d85a0d31b5712adc16b
John McDonnell <john.mcdonnell439@gmail.com>
2018-07-20 23:14:05 +0100

    adding std dev population

--- src/histogram/histogram.rs
+++ src/histogram/histogram.rs
@@ -1,21 +1,25 @@
 use std::time::Duration;
-use std::ops::{Div, Mul, Add};
+use std::ops::{Add, Sub, Div, Mul, Index};
 
 #[derive(Debug)]
 pub struct Histogram {
     durations: Vec<Duration>,
+    durations_as_nano: Vec<i64>,
     cumulative: Duration,
     mean: Duration,
     harmonic_mean: Duration,
+    standard_deviation: Duration,
 }
 
 impl Histogram {
     pub fn new() -> Histogram {
         let mut histogram = Histogram {
             durations: fake_duration_data(),
+            durations_as_nano: Vec::new(),
             cumulative: Duration::new(0, 0),
             mean: Duration::new(0, 0),
             harmonic_mean: Duration::new(0, 0),
+            standard_deviation: Duration::new(0, 0),
         };
         histogram.calculate();
         histogram
@@ -23,6 +27,8 @@ impl Histogram {
 
     pub fn add(&mut self, duration: Duration) {
         self.durations.push(duration);
+        self.durations_as_nano.push(nanoseconds(duration));
+        self.length();
         self.calculate();
     }
 
@@ -31,6 +37,7 @@ impl Histogram {
             self.cumulative();
             self.mean();
             self.harmonic_mean();
+            self.standard_deviation();
         }
     }
 
@@ -54,6 +61,27 @@ impl Histogram {
         let nanos = { self.length() as f64 }.div(sum_reciprocals) as u64;
         self.harmonic_mean = Duration::from_nanos(nanos);
     }
+
+    fn standard_deviation(&mut self) {
+        if self.length() < 2 {
+            return;
+        }
+        let mut sum_diff: i128 = 0;
+        let mean = nanoseconds(self.mean);
+        for duration in self.durations_as_nano.iter() {
+            let diff = duration.sub(mean);
+            let product_diff = { diff as i128 }.mul(diff as i128);
+            sum_diff = sum_diff.add(product_diff);
+        }
+        println!("{:?}", sum_diff);
+        let std_dev = { sum_diff.div((self.length() - 1) as i128) as f64 }.sqrt();
+        self.standard_deviation = Duration::from_nanos(std_dev as u64);
+    }
+
+    pub fn percentile(&mut self, p: f64) -> Duration {
+        let index = { { self.length() as f64 }.mul(p.add(0.5)) } as usize - 1;
+        *self.durations.index(index)
+    }
 }
 
 fn fake_duration_data() -> Vec<Duration> {
@@ -62,6 +90,9 @@ fn fake_duration_data() -> Vec<Duration> {
     data
 }
 
-fn nanoseconds(d: Duration) -> u64 {
-    d.as_secs().mul(1_000_000_000) + d.subsec_nanos() as u64
+fn nanoseconds(d: Duration) -> i64 {
+    let secs = d.as_secs().mul(1_000_000_000);
+    let nanos = d.subsec_nanos();
+    let total = secs + nanos as u64;
+    total as i64
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ mod histogram;
 
 
 fn main() {
-
     let mut h = histogram::histogram::Histogram::new();
     h.add(Duration::new(10, 0));
     h.add(Duration::new(20, 0));
     h.add(Duration::new(30, 0));
+
     println!("{:?}", h);
+    println!("{:?}", h.percentile(0.50));
 }
